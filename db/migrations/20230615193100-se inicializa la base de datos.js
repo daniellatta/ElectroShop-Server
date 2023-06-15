644d90a6071cc4093c5dec9e9f07f236202f9f0e
Bixxo <110485831+Bixxo@users.noreply.github.com>
2023-06-15 13:41:40 -0600

    cors

--- db/migrations/20230615193100-se inicializa la base de datos.js
+++ db/migrations/20230615193100-se inicializa la base de datos.js
@@ -1,18 +1,22 @@
 'use strict';
-
 const { CATEGORY_TABLE, CategorySchema } = require('../models/category.model');
 const { PRODUCT_TABLE, ProductSchema } = require('../models/products.model');
-
+const { REVIEW_TABLE, ReviewSchema } = require('../models/review.model');
+const { USER_TABLE, UserSchema } = require('../models/user.model');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface) {
     await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
     await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
+    await queryInterface.createTable(USER_TABLE, UserSchema);
+    await queryInterface.createTable(REVIEW_TABLE, ReviewSchema);
   },
 
   async down (queryInterface) {
-    await queryInterface.dropTable(CATEGORY_TABLE);
+    await queryInterface.dropTable(REVIEW_TABLE);
+    await queryInterface.dropTable(USER_TABLE);
     await queryInterface.dropTable(PRODUCT_TABLE);
+    await queryInterface.dropTable(CATEGORY_TABLE);
   }
 };
